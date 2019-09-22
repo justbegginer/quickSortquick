9778862eb8885350fe544247ca9c8c02b50febdf
justbegginer <46086383+justbegginer@users.noreply.github.com>
2019-09-22 23:10:54 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array={2,3,6,28 , 13 , 103 , 24 , 25};
+        int[] result = quickSort(array);
+        for(int i : result) {
+            System.out.println(i+" ");
+        }
+    }
+    private static int[] quickSort(int[] array){
+        int[] result ;
+        if (array.length < 2){
+            return array;
+        }
+        else {
+            int pivot = array[0];
+            List greatest = new ArrayList(),less = new ArrayList();
+            for (int i = 1 ;i<array.length ; i++){
+                if (array[i] > pivot){
+                    greatest.add(array[i]);
+                }
+                else{
+                    less.add(array[i]);
+                }
+
+            }
+            less = arrayToList(quickSort(listToArray(less)));
+            less.add(pivot);
+            greatest = arrayToList(quickSort(listToArray(greatest)));
+            less.addAll(greatest);
+            result = listToArray(less);
+        }
+        return result;
+    }
+    private static int[] listToArray(List inputList){
+        int[] resultArray = new int[inputList.size()];
+        for (int i = 0; i < resultArray.length; i++) {
+            resultArray[i] =(int) inputList.get(i);
+        }
+        return resultArray;
+    }
+    private static List arrayToList(int[] inputArray){
+        List result = new ArrayList();
+        for (int i : inputArray) {
+            result.add(i);
+        }
+        return  result;
+    }
+}
